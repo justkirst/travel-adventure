29494e4873c2cd88510e7eddcca01503425303a9
justkirst <89128835+justkirst@users.noreply.github.com>
2022-01-30 11:33:06 +0000

    Add start of game input and intro to game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Game Introduction
+name = input("What is your name?\n")
+print("Hi " + name + "!")
+print("You have just won the lottery and decided to go on an adventure and run straight to the airport for the next available flight.")
+print("You have two choices when you arrive at the airport.")
+destination = input("Do you want to go to a Tropical Island or a Dark Rainforest?\nTropical Island or Dark Rainforest\n")
+
+#Start of the Game
